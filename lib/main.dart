70a2e7af0f1993ae221425262f340e10dfab51cf
Shankar-CSE <shankar.l5252@gmail.com>
2025-07-14 17:57:03 +0530

    update: sample working model

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:ai_chat/screens/chat_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:ai_chat/services/chat_service.dart';
+import 'services/chat_service.dart';
+import 'screens/chat_screen.dart';
 
 void main() {
-  
   runApp(
-    Provider(create: (context) => ChatService("Api-key"),
-    child:const MyApp()),
-    );
-    
+    Provider(
+      create: (context) => ChatService('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'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); @override
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeepSeek Chatbot',
@@ -21,6 +23,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const ChatScreen(),
-      );
-}
+    );
+  }
 }
